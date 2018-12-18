762372f88b2f027d021caf629f477d75516eeee5
ranguiquan <673227233@qq.com>
2018-12-18 08:42:47 +0800

    全排列

--- practice/permute.c
+++ practice/permute.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 //全排列
 int r = 4;
-char data[4] = {'a', 'b', 'c', 'd'};
-int used[4] = {0,0,0,0};
-char p[4];
-void permute(int pos)
+char data[4] = {'a', 'b', 'c', 'd'};//带排元素
+int used[4] = {0,0,0,0};//标记元素是否被使用
+char p[4];//排列结果
+void permute(int pos)//pos:position 当前排的第几个元素
 {
     int i = 0;
     if(pos == r)
@@ -16,12 +16,13 @@ void permute(int pos)
         printf("\n");
         return;
     }
-    for(i = 0; i < r; i++){
+    //递归边界
+    for(i = 0; i < r; i++){//遍历当前可用元素
         if(!used[i]){
-            used[i]++;
+            used[i]++;//标记为已用
             p[pos] = data[i];
             permute(pos+1);
-            used[i]--;
+            used[i]--;//退出递归后标记为未用
         }
     }
 }
